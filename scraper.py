72a85e0304a5e12c770eadc12abc1bf439325622
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-02-16 13:07:38 +0100

    created function that returns a table with the travel information
     Creates a table with hour of departure, of arrival to destination, travel time, if only SJ or if there are other companies involved and price
     If any of the rows has a null value, that row is removed from the table
    Takes soup and the class of the table as argument

--- scraper.py
+++ scraper.py
@@ -4,50 +4,39 @@ from selenium import webdriver
 import time
 import numpy as np
 
-def scraper(driver):
-    time.sleep(5)
 
-    html = driver.page_source
-    soup = bs.BeautifulSoup(html, "lxml")
-
-
-    time_schedule = soup.find_all("div", {'class':'timetable__time-info timetable__time-info--small guttered--quarter-vertically ng-binding'})
-    nrows =0
-    ncols = 5
-    for t in time_schedule:
-        nrows+=1
-
-    table_information = np.empty((nrows, ncols), dtype=object)
-
-    prices = soup.find_all('div', {'class':'timetable-cell timetable-cell__right-cell timetable-cell__unexpanded timetable-cell__unexpanded-class timetable-cell__class ng-isolate-scope'})
-    operator = soup.find_all('div', {'class':'timetable__extra-info-icon'})
-
-    travel_time = soup.find_all("span",{'class':'ng-isolate-scope ng-binding'})
-
+def create_table(soup,table_class):
+    '''
+    Creates a table with hour of departure, of arrival to destination, travel time, if only SJ or if there are other companies involved and price
+    If any of the rows has a null value, that row is removed from the table
+    '''
+    page_table = soup.find("div", {"class":table_class})
+    travel_hours = page_table.find_all("div", "timetable__time-info timetable__time-info--small guttered--quarter-vertically ng-binding")
+    prices = page_table.find_all('div', {'class':'timetable-cell timetable-cell__right-cell timetable-cell__unexpanded timetable-cell__unexpanded-class timetable-cell__class ng-isolate-scope'})
+    travel_time = page_table.find_all("span",{'class':'ng-isolate-scope ng-binding'})
     travel_t=[]
     for t in range(0,len(travel_time),2):
         travel_t.append(travel_time[t].text)
-        #print(travel_t[t])
-    h1=[]
-    h2=[]
-    for t in range(len(time_schedule)):
-        hours = time_schedule[t].text.replace("\n","").strip()
-        hours = hours.replace(":", "")
-        h1.append(hours[:4])
-        #h2.append(hours[7:])
 
-    t=1
-    while h1[t-1]<=h1[t]:
-        print(h1[t-1]+"=<"+h1[t])
-        t+=1
-    for t in range(len(time_schedule)):
-        hours = time_schedule[t].text.replace("\n","").strip()
-        hours = hours.replace(":", "")
+    operator = page_table.find_all('div', {'class':'timetable__extra-info-icon'})
 
-        table_information[t][0] = hours[:4]
-        table_information[t][1] = hours[7:]
+    nrows = len(travel_hours)
+    ncols = 5
+    arr = np.empty((nrows, ncols), dtype=object)
 
-        price = prices[t].text.replace("\n","").strip()
+    for i in range(nrows):
+        hours = travel_hours[i].text.replace("\n","").strip()
+        arr[i][0] = hours[:5]
+        arr[i][1] = hours[8:]
+
+        arr[i][2] = travel_t[i].replace(" h", "")
+
+        op = operator[i].text.replace("\n","").strip()
+        if "+0" in op:
+            op = op.replace("+0","")
+        arr[i][3] = op
+
+        price = prices[i].text.replace("\n","").strip()
         price = price.replace("fr.","")
         price = price.split(":")[0]
         price = price.replace(" ","")
@@ -56,24 +45,25 @@ def scraper(driver):
         except:
             price=None
 
-        table_information[t][4] = price
+        arr[i][4] = price
 
-        op = operator[t].text.replace("\n","").strip()
-        if "+0" in op:
-            op = op.replace("+0","")
-        table_information[t][3] = op
-
-        table_information[t][2] = travel_t[t].replace(" h", "")
-
-    print(table_information)
-    print()
     #remove rows with None
-    #mask = np.any(np.equal(table_information, None), axis=1)
-    #print(table_information[~mask])
-    #print()
-    #driver.quit()
+    mask = np.any(np.equal(arr, None), axis=1)
+    arr = (arr[~mask])
+    return(arr)
+
+def scraper(driver):
+    time.sleep(5)
+
+    html = driver.page_source
+    soup = bs.BeautifulSoup(html, "lxml")
+
+    departure_table = create_table(soup, "guttered--double-bottom guttered--mobile-bottom ng-isolate-scope")
+    print(departure_table)
+    print()
+    arrival_table = (create_table(soup,"timetable-inbound guttered--double-bottom guttered--mobile-bottom ng-scope ng-isolate-scope"))
+    print(arrival_table)
+
+    driver.quit()
 
 #sorted([list]) sorts even with separator like ":"
-
-
-#guttered--double-bottom guttered--mobile-bottom ng-isolate-scope
